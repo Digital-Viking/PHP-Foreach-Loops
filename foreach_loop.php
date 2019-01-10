9918fcf05c4b381bb3b0f395fc2f7dac1df8f491
David Weston Webb <westwebb@gmail.com>
2019-01-10 11:30:04 -0500

    Create foreach_loop.php

---
+++ foreach_loop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$numbers = 
+array(234,6524,6746,2344,1234,5452,6352,2431,41645);
+
+foreach($numbers as $number){
+    
+    echo $number . "<br>";
+
+}
+
+?>
+
+</body>
+</html>
